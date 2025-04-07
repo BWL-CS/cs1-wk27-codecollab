35d4c29c8f5d4b2e771ed766d67514414af53e79
Katerina Walter <94501766+KaterinaWalter@users.noreply.github.com>
2025-04-07 10:49:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("hello world");
+// THIS IS A JS COMMENT (like a note to yourself)
+// The comments in this program are used to identify different sections
+// Pick one of the sections to focus on in your discussion post
+
+
